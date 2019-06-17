e134f0c1be66515013deccfbcd41f3cf8ea9c7a4
Ahmed El-Kollaly <ahmed_elkollaly@hotmail.com>
2019-06-17 14:18:31 +0200

    Initial Commit

---
+++ PhotoExplorer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoExplorer
+//
+//  Created by Ahmed El-Kollaly on 6/17/19.
+//  Copyright © 2019 Ahmed El-Kollaly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhotoExplorer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoExplorer
+//
+//  Created by Ahmed El-Kollaly on 6/17/19.
+//  Copyright © 2019 Ahmed El-Kollaly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
